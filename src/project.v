e9f53638f046c6f964f5b4110d5af5c37cd9d8b3
yyuysal <yuysal@doveschools.org>
2025-06-18 10:42:41 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,20 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+ // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+
+    tt_um_and top(
+        .clk(clk),
+        .reset(rst_n),
+	.a(ui_in[7:0]),
+	.b(uio_in[7:0]),
+	.Y(uo_out[7:0])
+	);
+
 
 endmodule
